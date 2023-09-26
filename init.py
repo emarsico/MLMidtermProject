2c56937a69b7e6f5cb22ff3b65ffaf3d2a9094f0
mls6888 <mls6888@psu.edu>
2023-09-26 15:51:15 -0400

    hello

--- init.py
+++ init.py
@@ -4,20 +4,36 @@ import pandas as pd
 import torch
 import nltk
 
+
 # 2. Load data
-def load_data():
-    # TODO: Implement data loading functionality
-    pass
+def load_data(file_name):
+    words = []
+    pos_tags = []
+    entity_labels = []
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+
+    for line in lines:
+        parts = line.strip().split()
+        if len(parts) == 3:
+            word, pos, entity = parts
+            words.append(word)
+            pos_tags.append(pos)
+    df = pd.DataFrame({"Word": words, "POS_Tag": pos_tags})
+    return df
+
 
 # 3. Preprocess data
 def preprocess_data(data):
     # TODO: Implement data preprocessing steps
     pass
 
+
 # 4. Split data into training and testing sets
 def split_data(data, labels):
     # TODO: Implement data splitting
     pass
+
 
 # 5. Define the ML model
 class CustomModel:
@@ -33,22 +49,27 @@ class CustomModel:
         # TODO: Implement prediction procedure
         pass
 
+
 # 6. Train the model
 def train_model(model, X_train, y_train):
     # TODO: Implement the model training
     pass
+
 
 # 7. Evaluate the model
 def evaluate_model(model, X_test, y_test):
     # TODO: Implement model evaluation
     pass
 
+
 # 8. Main execution
 if __name__ == "__main__":
     # Load data
-    data, labels = load_data()
+    lines = load_data("train.txt")
+    print(lines.head)
 
     # Preprocess data
+    """
     processed_data = preprocess_data(data)
 
     # Split data
@@ -60,3 +81,4 @@ if __name__ == "__main__":
 
     # Evaluate the model
     evaluate_model(model, X_test, y_test)
+    """
