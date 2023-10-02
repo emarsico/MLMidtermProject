f94a8b0f56930794a8cfbd796c11c57e4d5d58a0
mls6888 <mls6888@psu.edu>
2023-10-02 13:34:41 -0400

    commit

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ from math import floor
 import random
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import torch
 import nltk
 from nltk.classify import NaiveBayesClassifier
@@ -118,6 +119,30 @@ def distance_from_period(text):
     return l
 
 
+# Not done
+def distances_from_end_of_sentence(text):
+    result = []
+    for sentence in np.array_split(text, np.where(text == ".")[0] + 1):
+        for i, word in enumerate(sentence):
+            distance_from_end = len(sentence) - i - 1
+            result.extend([distance_from_end])
+    return result
+
+
+def syllable_count(word):
+    word = word.lower()
+    count = 0
+    vowels = ["a", "e", "i", "o", "u"]
+    if word[0] in vowels:
+        count += 1
+    for index in range(1, len(word)):
+        if word[index] in vowels and word[index - 1] not in vowels:
+            count += 1
+    if word.endswith("e"):
+        count -= 1
+    return count if count != 0 else 1
+
+
 # Split data into training and testing sets
 def split_data(data, labels):
     # TODO: Implement data splitting
@@ -155,7 +180,6 @@ def evaluate_model(model, X_test, y_test):
 if __name__ == "__main__":
     # Load data
     data = load_data("train.txt")
-    print(data.head)
     # print(set(Y.values))
     # unique_labels = set(Y.values)
     # d = numerical_labels(unique_labels)
@@ -164,24 +188,41 @@ if __name__ == "__main__":
     data["Length"] = data["Word"].apply(len)
     data["Prefix"] = data["Word"].apply(prefix)
     data["Suffix"] = data["Word"].apply(suffix)
+    data["Syllables"] = data["Word"].apply(syllable_count)
     distances = distance_from_period(data["Word"].values)
+    distance_from_end = distances_from_end_of_sentence(data["Word"].values)
+    print(distance_from_end)
     for index, row in data.iterrows():
         data.at[index, "Position"] = floor(distances[index])
-
-    print(data.columns)
-    X = data[["Word", "Capitalized", "Length", "Prefix", "Suffix", "Position"]]
+    for index, row in data.iterrows():
+        data.at[index, "Distance_From_End"] = floor(distance_from_end[index])
+    X = data[
+        [
+            "Word",
+            "Capitalized",
+            "Length",
+            "Prefix",
+            "Suffix",
+            "Position",
+            "Distance_From_End",
+        ]
+    ]
+    # X = data[["Word"]]
     Y = data["POS_Tag"]
-    featuresets = [
+    features = [
         ({col: row[col] for col in X.columns}, label)
         for index, (index, row), label in zip(X.iterrows(), data.iterrows(), Y)
     ]
-    random.shuffle(featuresets)
+    random.shuffle(features)
 
     # Split data into training and testing sets
+    """
     train_set, test_set = (
-        featuresets[: int(len(featuresets) * 0.8)],
-        featuresets[int(len(featuresets) * 0.8) :],
+        features[: int(len(features) * 0.8)],
+        features[int(len(features) * 0.8) :],
     )
+    """
+    train_set, test_set = train_test_split(features, test_size=0.2, random_state=42)
 
     # Train
     classifier = NaiveBayesClassifier.train(train_set)
